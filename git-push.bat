370f89e4d5df72ecb963ff16c627ab126b0439df
Truong Vinh Phuc <truongphuc05112004@gmail.com>
2026-03-01 16:50:06 +0700

    remove bat files

--- git-push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Running Git Commands...
-git checkout -b feature/courses
-git add .
-git commit -m "feat: implement Course Master module (CRUD, Curriculum Builder, Media Resources)"
-git push -u origin feature/courses
-echo Done.
-pause
